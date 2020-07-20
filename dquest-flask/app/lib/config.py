66e2ff161a57d701231c8a63e4a8f4c3fb949c62
Yingcheng Sun <yxs489@case.edu>
2020-07-20 00:40:55 -0400

    Add files via upload

---
+++ dquest-flask/app/lib/config.py
@@ -0,0 +1,14 @@
+CRITERIA_HOST = '0.0.0.0'
+CRITERIA_DATABASE = ''
+CRITEIRA_USERNAME = ''
+CRITERIA_PASSWORD = ''
+CRITERIA_DRIVER = '{ODBC Driver 17 for SQL Server}' # make sure the driver is installed on the server
+CRITERIA_PORT  = 
+
+AACT_HOST = 'aact-db.ctti-clinicaltrials.org'
+AACT_PORT = 
+AACT_DATABASE = 'aact'
+AACT_USERNAME = ''
+AACT_PASSWORD = ''
+CSRF_ENABLED = True
+SECRET_KEY = 'XXX'
---
+++ dquest-flask/run.py
@@ -0,0 +1,5 @@
+from app import app
+
+# launch
+if __name__ == '__main__' :
+    app.run (debug=True)
